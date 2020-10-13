0ac7195568eb2b203c09339d45d6491a2a010542
Carlos Vinicius <vinicius26092000@gmail.com>
2020-10-12 21:53:24 -0300

    first commit on nlw 03

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(routes);
+
+export { app };
